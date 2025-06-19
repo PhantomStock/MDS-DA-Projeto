eeb36ddf8a13549354ecd028caaea329741862ae
Wilson Campos <2240115@my.ipleiria.pt>
2025-06-19 20:27:11 +0100

    Model Sessao Atual

---
+++ Projeto-DA-MDS_iTasks/Models/SessaoAtual.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTasks.Models
+{
+    class SessaoAtual
+    {
+        public static Utilizador Utilizador { get; set; }
+
+
+
+    }
+}
